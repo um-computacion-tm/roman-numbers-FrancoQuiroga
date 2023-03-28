c6bff207e2145782afa27385fb05f89a1be0a5e5
FrancoQuiroga <fe.quiroga@gmail.com>
2023-03-27 22:12:49 -0300

    Prueba 2

--- pythonfacul/Decimalaromanos/decimaltoroman.py
+++ pythonfacul/Decimalaromanos/decimaltoroman.py
@@ -1,16 +1,16 @@
 import unittest
 
 def decimal_to_roman(decimal):
-    if decimal <= 3:
-        return 'I' * decimal
-    elif decimal == 4:
-        return 'IV'
-    elif decimal == 5:
-        return 'V'
-    elif decimal == 10:
-        return "X"
-
-
+    valoresromano = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1] #lista de numeros para restarle a (decimal)
+    letrasromano = ['M', 'CM', 'D', ' CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'] #Lista de letras para agregarle a total
+    total = ''
+    while decimal > 0:
+        i = 0
+        for _ in range(decimal // valoresromano[i]):
+            total += letrasromano[i]
+            decimal -= valoresromano[i]
+        i += 1
+    return total
 class TestDecimalToRoman(unittest.TestCase):
     def test_uno(self):
         # pre condicion
@@ -35,9 +35,11 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_tres(self):
         resultado = decimal_to_roman(3)
         self.assertEqual(resultado, 'III')
-    def test_cuatro(self):
-        resultado = decimal_to_roman(4)
-        self.assertEqual(resultado,'IV')
-
+    def test_cientotres(self):
+        resultado = decimal_to_roman(103)
+        self.assertEqual(resultado,'CIII')
+    def test_124(self):
+        resultado = decimal_to_roman(124)
+        self.assertEqual(resultado, 'CXXIII')
 if __name__ == '__main__':
     unittest.main()
